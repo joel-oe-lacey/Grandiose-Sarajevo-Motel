8cdc36e30a58c0cd9a112c83223227e74d636b3a
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-11 14:32:50 -0700

    Add user method to test for total spent

--- classes/user.js
+++ classes/user.js
@@ -8,7 +8,17 @@ class User {
     return hotel.bookings.filter(booking => booking.userID === this.id);
   }
 
-  
+  calculateRewardsTotal(hotel) {
+    const userRes = this.findReservations(hotel);
+    return userRes.reduce((acc, res) => {
+      hotel.rooms.forEach(room => {
+        if (room.number === res.roomNumber) {
+          acc += room.costPerNight;
+        }
+      });
+      return acc;
+    }, 0)
+  }
 }
 
 export default User;
