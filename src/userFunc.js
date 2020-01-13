85d349214e1b25a1b0ad7780d40b0bc551399b05
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-13 10:10:21 -0700

    Restructure data fetch to single promise.all return

--- src/userFunc.js
+++ src/userFunc.js
@@ -8,58 +8,51 @@ const validateLogin = (e) => {
   const username = $('.login-username').val();
   const password = $('.login-password').val();
 
-  generateUserPage();
-  displayBookings();
-  // if (username && password === 'overlook2019') {
-  //   generateUserPage();
-  //   // unpackData(username);
-  // } else {
-  //   console.log('Please enter value');
-  // }
+  if (username && password === 'overlook2019') {
+    retrieveUserData(username);
+    generateUserPage();
+    displayBookings();
+  } else {
+    console.log('Please enter value');
+  }
 }
 
 $('.login-submit').on('click', validateLogin);
 
 const linkUser = (username, userData) => {
   const userID = username.match(/\d+/)[0];
-  const userInfo = userData.find(user => user.id === userID);
+  const userInfo = userData.find(user => user.id === parseInt(userID));
+  console.log('userFind', userInfo)
   return new User(userInfo);
 }
 
-const loadDashboard = (user, hotel) => {
-    console.log(user)
-    console.log(hotel)
+const unpackData = (username, data) => {
+  const user = linkUser(username, data[0].users);
+  const bookings = data[2].bookings.map(booking => new Booking(booking));
+  const hotel = new Hotel('Grandiose Sarajevo Motel', data[1].rooms, bookings);
+
+  console.log('bookings', bookings)
+  console.log('hotel', hotel)
+  console.log('user', user)
 }
 
-const unpackData = (username) => {
-  const data = retrieveUserData();
-  console.log(data)
-  const bookings = bookingData.map(booking => new Booking(booking));
-  const hotel = new Hotel('Grandiose Sarajevo Motel', roomData, bookings);
-  loadDashboard(user, hotel)
-}
-
-const retrieveUserData = async() => {
-  let urls = {
+const retrieveUserData = (username) => {
+  const urls = {
     userData: 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users',
     roomData: 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms',
     bookingData: 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings',
-  }
-  const dataFetches = Object.keys(urls).map(url => fetch(urls[url]));
+  };
 
-  //await the result of promise all 
-  const data = await Promise.all(dataFetches)
-    .then(responses => responses.map(response => response.json()))
-    // .then(resolved => {
-    //   resolved.forEach(dataSet => {
-    //     dataSet.then(data => {
-    //       hotelData.push(Object.entries(data))
-    //     })
-    //   })
-    .catch(err => console.log(err))
-    //throw exception on catch, offer alert on UI to refresh, dont proceed until loaded 
-
-  return data;
+  const dataFetches = Object.keys(urls).map(url => {
+    return fetch(urls[url])
+      .then(response => response.json())
+  });
+  
+  Promise.all(dataFetches)
+    .then(data => {
+      console.log('dataBefore', data)
+      unpackData(username, data);
+    })
 }
 
 const generateUserPage = () => {
@@ -71,7 +64,7 @@ const generateUserPage = () => {
         <button class="nav-reservation nav-btn">Make A Reservation</button>
         </nav>
       <section class="dashboard">
-        <p class="dashboard-hold">Under Construction</p>
+      
       </section>
     </section>`);
 }
