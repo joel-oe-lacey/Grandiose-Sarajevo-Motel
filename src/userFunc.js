9c009c0eb9242dd8c992cd86282bcb074363fde8
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-13 14:35:05 -0700

    Add user sections and styling

--- src/userFunc.js
+++ src/userFunc.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import User from '../classes/user.js';
 import Hotel from '../classes/hotel.js';
 import Booking from '../classes/booking.js';
+let user, bookings, hotel;
 
 const validateLogin = (e) => {
   e.preventDefault()
@@ -10,8 +11,6 @@ const validateLogin = (e) => {
 
   if (username && password === 'overlook2019') {
     retrieveUserData(username);
-    generateUserPage();
-    displayBookings();
   } else {
     console.log('Please enter value');
   }
@@ -26,13 +25,16 @@ const linkUser = (username, userData) => {
 }
 
 const unpackData = (username, data) => {
-  const user = linkUser(username, data[0].users);
-  const bookings = data[2].bookings.map(booking => new Booking(booking));
-  const hotel = new Hotel('Grandiose Sarajevo Motel', data[1].rooms, bookings);
+  const userInst = linkUser(username, data[0].users);
+  const bookingInst = data[2].bookings.map(booking => new Booking(booking));
+  const hotelInst = new Hotel('Grandiose Sarajevo Motel', data[1].rooms, bookingInst);
 
-  console.log('bookings', bookings)
-  console.log('hotel', hotel)
-  console.log('user', user)
+  bookings = bookingInst;
+  hotel = hotelInst;
+  user = userInst;
+  generateUserPage();
+  // selectedBtnStyle('nav-booking');
+  displayUserResv();
 }
 
 const retrieveUserData = (username) => {
@@ -57,28 +59,40 @@ const generateUserPage = () => {
   $('body').html(`<section class="main">
     <nav class="nav">
       <img class="nav-logo" src="./images/hotel-logo.svg" alt="A hotel on top of a mountain with a train ascending to it">
-        <button class="nav-booking nav-btn">Your Bookings</button>
+        <button class="nav-reservation nav-btn">Your Bookings</button>
         <button class="nav-account nav-btn">Your Account</button>
-        <button class="nav-reservation nav-btn">Make A Reservation</button>
+        <button class="nav-booking nav-btn">Make A Reservation</button>
         </nav>
-      <section class="dashboard">
-      
+      <section class="dash">
+      <section class= 'dash-card-list' ></section>
       </section>
     </section>`);
 }
 
-const createResCard = () => {
-  //need to use .after on .card unless it's the first card?
-  $('dash').html(`<section class='dash-card'>
+const displayUserResv = () => {
+  generateUserPage()
+  const userResInst = user.findPersonalReservations(hotel);
+  createUserRes(userResInst);
+}
+
+const createResCard = (resDate, roomNum, roomType) => {
+  $('.dash-card-list').append(`<section class='dash-card'>
     <img class="dash-img" src="./images/room-logo.svg" alt="Front facing view of an opulent hotel">
-    <p class="dash-p-resDate">2020/06/01</p>
-    <p class="dash-p-roomType">Single Room</p>
+    <p class="dash-p-resDate">${resDate}</p>
+    <p class="dash-p-roomNum">Room Number: ${roomNum}</p>
+    <p class="dash-p-roomType">${roomType}</p>
     </section>`);
 }
 
+const createUserRes = (bookings) => {
+  bookings.forEach(booking => createResCard(booking.date, booking.roomNumber, 'test'))
+}
+
+$(document).on('click', '.nav-reservation', displayUserResv);
+
 const displayUserStats = () => {
   //need to build out more with more info, styling dependent on time
-  $('dash').html(`<section class="dash">
+  $('.dash').html(`<section class="dash">
     <section class="dash-rewards">
         <h3 class="rewards-h3">75%</h3>
     </section>
@@ -88,6 +102,28 @@ const displayUserStats = () => {
     </section>`);
 }
 
-const displayBookings = () => {
-  $('.dashboard').html(`<p class="dashboard-hold">test</p>`);
+$(document).on('click', '.nav-account', displayUserStats);
+
+const generateBookingPage = () => {
+  $('.dash').html(`<form class="dash-form">
+    <label for="date">Select a date:</label>
+    <input class="dash-input-date" name="date" type="date">
+    <label for="roomType">Filter by type:</label>
+    <select class="dash-select" name="roomType" id="">
+        <option value="junior suite">Junior Suite</option>
+        <option value="residential suite">Residential Suite</option>
+        <option value="suite">Suite</option>
+        <option value="single room">Single Room</option>
+    </select>
+  </form>
+  <section class="dash-results">
+  </section>`);  
 }
+
+$(document).on('click', '.nav-booking', generateBookingPage);
+
+// const selectedBtnStyle = (btn) => {
+//   //this function is to preserve the site button styling without removing focus styles on the page switching buttons 
+//   $(`.${btn}`).css({
+//     "background": "linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .7))", "border": "none"});
+// }
